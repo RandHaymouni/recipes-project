19c37124d4d34f160a4e3a02ca00933477b8b06b
Rand Haymouni <rand54144@gmail.com>
2024-07-11 22:26:45 +0300

    creating the home page

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-const Card = () => {
+const Card = ({ recipe }) => {
+    const navigate = useNavigate();
+
+    const handleButtonClick = (id) => {
+        navigate(`/recipe/${id}`);
+    };
+
     return (
-        <div>Card</div>
+        <div className="overflow-hidden flex flex-col w-72 mt-4 bg-[#f3f3f3] rounded shadow-xl hover:scale-105 hover:shadow-xl">
+            <div className="relative">
+                <img className="w-full"
+                    src={recipe.image}
+                    alt={recipe.name} />
+                <div
+                    className="hover:bg-transparent transition duration-300 absolute bottom-0 top-0 right-0 left-0 bg-gray-900 opacity-25">
+                </div>
+                <button
+                    onClick={() => handleButtonClick(recipe.id)}
+                    className="text-xs absolute top-0 right-0 bg-indigo-600 px-4 py-2 text-white mt-3 mr-3 hover:bg-white hover:text-indigo-600 transition duration-500 ease-in-out">
+                    Cooking
+                </button>
+            </div>
+            <p className="font-medium text-lg hover:text-indigo-600 transition duration-500 ease-in-out flex justify-center text-black mt-3">
+                {recipe.name}
+            </p>
+            <div className="flex justify-between items-center m-3">
+                <div className="flex flex-col">
+                    <span className="text-gray-500 text-sm">Difficulty</span>
+                    <span className="text-black font-medium">{recipe.difficulty}</span>
+                </div>
+                <div className="flex flex-col">
+                    <span className="text-gray-500 text-sm flex items-cente justify-center">Rating</span>
+                    <div className="flex gap-2">
+                        <span className="text-black font-medium flex items-center justify-center">{recipe.rating}</span>
+                        <span className="text-gray-500">/5</span>
+                    </div>
+                </div>
+                <div className="flex flex-col">
+                    <span className="text-gray-500 text-sm flex items-center justify-center">Cuisine</span>
+                    <span className="text-black font-medium">{recipe.cuisine}</span>
+                </div>
+            </div>
+        </div>
     )
 }
 
-export default Card
+export default Card;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import cookingBlog from '../assets/cookingBlog.svg';
+import Card from "../components/Card";
 
-const Home = () => {
+const Home = ({ recipes }) => {
     return (
-        <div>Home</div>
+        <div>
+            <div className="flex-col m-4 gap-x-3">
+                <div className="mb-7">
+                    <h1 className="justify-center items-center text-center text-black font-normal text-7xl mt-11">
+                        Discover Delicious
+                    </h1>
+                    <h2 className="flex justify-center items-center text-indigo-600 font-extrabold text-6xl">Recipes</h2>
+                </div>
+                <p className="text-center text-gray-500 text-lg flex-wrap">
+                    Explore a world of flavors and create mouthwatering dishes with our collection of easy-to-follow recipes.
+                </p>
+            </div>
+            <div className="border-b mb-5 flex justify-between text-sm m-14">
+                <div className="text-indigo-600 flex items-center pb-2 pr-2 border-b-2 border-indigo-600 uppercase mx-20">
+                    <img src={cookingBlog} alt="Cooking Blog" className="h-6 mr-3" />
+                    <span className="font-semibold flex items-center">Cooking BLog</span>
+                </div>
+            </div>
+            <div className="flex justify-center items-center gap-5 flex-wrap mx-6">
+                {
+                    recipes.map((recipe) => {
+                        return (
+                            <Card recipe={recipe} key={recipe.id} />
+                        )
+                    })
+                }
+            </div>
+        </div>
     )
 }
 
-export default Home
+export default Home;
