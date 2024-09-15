a5d431d717fde781340a0b1146080ac6df4bd608
Rand Haymouni <rand54144@gmail.com>
2024-09-16 00:54:10 +0300

    Update project title and logo

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
                     </p>
                 </div>
                 <div
-                    className="bg-gray-100 flex border-2 max-md:order-1 focus-within:border-indigo-500 focus-within:bg-transparent px-4 rounded-sm h-11 lg:w-2/4 max-md:w-full">
+                    className="bg-gray-100 flex border-2 max-md:order-1 focus-within:border-indigo-500 focus-within:bg-transparent px-4 rounded-sm h-11 lg:w-2/4 max-md:w-full justify-centerS items-center">
                     <svg
                         xmlns="http://www.w3.org/2000/svg"
                         viewBox="0 0 192.904 192.904"
